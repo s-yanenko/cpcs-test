c8c2ea809bff41560272cd2816ae7fdcdc3eb638
Serhii Yanenko <s.yanenko.se@gmail.com>
2020-05-11 20:25:34 +0300

    Basic Structure
    Added basic prepariations

---
+++ cpcs-test/cpcs-test/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cpcs-test
+//
+//  Created by Serhii Yanenko on 11.05.2020.
+//  Copyright © 2020 Serhii Yanenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ cpcs-test/cpcs-test/View Controllers/Weather/WeatherViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewController.swift
+//  cpcs-test
+//
+//  Created by Serhii Yanenko on 11.05.2020.
+//  Copyright © 2020 Serhii Yanenko. All rights reserved.
+//
+
+import Foundation
